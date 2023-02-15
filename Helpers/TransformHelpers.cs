8b392fb6ad48b681a4a16f7b25e0b3fc95fcb32a
Mikkel <Mikkel123sej@gmail.com>
2023-02-15 11:24:07 -0800

    added a transform helper ("ClosestScript") that can find the nearest item in a list of scripts/transforms from a transform

---
+++ Helpers/TransformHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Tools.Helpers
+{
+	public static class TransformHelpers
+	{
+		[CanBeNull]
+		public static T ClosestScript<T>(this Transform transform, IEnumerable<T> scriptsOfRightType)
+		where T : MonoBehaviour
+		{
+			T closestScript = null; // return null if empty list
+			float closestDistance = Mathf.Infinity;
+			Vector2 ourPos = transform.position.AsV2FromV3();
+			foreach (T scriptOfRightType in scriptsOfRightType)
+			{
+				Vector2 otherPos = scriptOfRightType.transform.position.AsV2FromV3();
+				float distance = Vector2.Distance(ourPos, otherPos);
+				if (distance >= closestDistance) continue;
+
+				closestScript = scriptOfRightType;
+				closestDistance = distance;
+			}
+			return closestScript;
+		}
+	}
+}

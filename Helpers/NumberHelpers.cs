98cc85078c29de8725068a6602a0b28337fde193
Mikkel <Mikkel123sej@gmail.com>
2023-03-05 00:06:21 +0100

    added stuff for range and numbers

--- Helpers/NumberHelpers.cs
+++ Helpers/NumberHelpers.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using Tools.Types;
 using UnityEngine;
 
 namespace Tools.Helpers
@@ -12,5 +13,6 @@ namespace Tools.Helpers
 		public static int AsSignedIntOrZero(this float value, float deadZone = 0f) => value.Abs() > deadZone ? value.AsIntSign() : 0;
 		public static float Abs(this float value) => Mathf.Abs(value);
 		public static float SignedMax(this float original, float modifier) => modifier.AsIntSign() * Mathf.Max(original.Abs(), modifier.Abs());
+		public static float RandomValueInRange(this Range<float> range) => Random.Range(range.Min, range.Max);
 	}
 }
--- Types/Range.cs
+++ Types/Range.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Tools.Types
 {

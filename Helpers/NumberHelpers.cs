9aed1baf7ddfa4c50d4557c7eb0de0cec5efe011
Mikkel <Mikkel123sej@gmail.com>
2023-03-05 02:57:43 +0100

    another range helper

--- Helpers/NumberHelpers.cs
+++ Helpers/NumberHelpers.cs
@@ -13,6 +13,8 @@ namespace Tools.Helpers
 		public static int AsSignedIntOrZero(this float value, float deadZone = 0f) => value.Abs() > deadZone ? value.AsIntSign() : 0;
 		public static float Abs(this float value) => Mathf.Abs(value);
 		public static float SignedMax(this float original, float modifier) => modifier.AsIntSign() * Mathf.Max(original.Abs(), modifier.Abs());
+
 		public static float RandomValueInRange(this Range<float> range) => Random.Range(range.Min, range.Max);
+		public static float MappedToRange(this Range<float> range, float value01) => Mathf.LerpUnclamped(range.Min, range.Max, value01);
 	}
 }

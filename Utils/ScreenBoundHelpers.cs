5e31933f006c29cd7aefbab0df40c69ebf9c7bee
Mikkel <Mikkel123sej@gmail.com>
2023-05-03 22:32:28 +0200

    made hacky helper for noticing if a thing is out of bounds, dunno if it works yet

---
+++ Utils/ScreenBoundHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+namespace Tools.Utils
+{
+	[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+	[SuppressMessage("ReSharper", "UnusedMember.Global")]
+	[SuppressMessage("ReSharper", "UnusedType.Global")]
+	public static class ScreenBoundHelpers
+	{
+		public static bool PointOutsideViewArea2D(this Camera cam, Vector2 pos, float clearance = 0f)
+		{
+			float height = cam.orthographicSize;
+			float width = height * cam.aspect;
+			float verticalClearance = height + clearance;
+			float horizontalClearance = width + clearance;
+			return pos.x.Abs() > verticalClearance || pos.y.Abs() > horizontalClearance;
+		}
+	}
+}

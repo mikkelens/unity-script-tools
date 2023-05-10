95e4f6d788a2edb8809b02421e9366bb85aacecf
Mikkel <Mikkel123sej@gmail.com>
2023-05-10 10:42:12 +0200

    added unscaled time helpers

--- Utils/TimeHelpers.cs
+++ Utils/TimeHelpers.cs
@@ -10,6 +10,9 @@ namespace Tools.Utils
 	{
 		public static float TimeSince(this float time) => Time.time - time;
 		public static float TimeUntill(this float time) => time - Time.time;
+		public static float TimeSinceUnscaled(this float unscaledTime) => Time.unscaledTime - unscaledTime;
+		public static float TimeUntillUnscaled(this float unscaledTime) => unscaledTime - Time.unscaledTime;
+
 		public static float DifferenceTo(this float a, float b) => Difference(a, b);
 		public static float Difference(float a, float b) => Mathf.Abs(a - b);
 	}

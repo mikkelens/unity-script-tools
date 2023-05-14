0949af83f1b013df847cb3e02ec362808daf8b4d
Mikkel <Mikkel123sej@gmail.com>
2023-05-14 05:44:49 +0200

    moved optional helper

---
+++ Utils/OptionalHelpers.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Tools.Types;
+
+namespace Tools.Utils
+{
+	[SuppressMessage("ReSharper", "UnusedType.Global")]
+	[SuppressMessage("ReSharper", "UnusedMember.Global")]
+	public static class OptionalHelpers
+	{
+		public static Optional<T> AsEnabled<T>(this T value) => new Optional<T>(value, true);
+		public static Optional<T> AsDisabled<T>(this T value) => new Optional<T>(value, false);
+	}
+}
